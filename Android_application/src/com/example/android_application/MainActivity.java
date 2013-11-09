f19eed36b2edf1747485a3df1fb1e07029e8cbbe
cbarreno <cbarreno@espol.edu.ec>
2013-11-09 13:15:18 -0500

    Primeras pantallas del juego buscaminas: Menú y Datos de Usuario
    Pantalla de Menu y Pantalla de recolección de datos del jugador

---
+++ Android_application/src/com/example/android_application/MainActivity.java
@@ -0,0 +1,53 @@
+package com.example.android_application;
+
+import android.os.Bundle;
+import android.app.Activity;
+import android.content.Intent;
+import android.view.View.OnClickListener;
+import android.view.Menu;
+import android.view.View;
+import android.widget.Button;
+
+public class MainActivity extends Activity implements OnClickListener
+{
+
+	Button btStart,btInstrucciones,btRecord,btExit;
+	
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+        
+        btStart=(Button)findViewById(R.id.btstart);
+        btInstrucciones=(Button)findViewById(R.id.btInstrucciones);
+        btRecord=(Button)findViewById(R.id.btRecord);
+        btExit=(Button)findViewById(R.id.btExit);
+        
+        btStart.setOnClickListener(this);
+       
+    }
+
+
+    @Override
+    public boolean onCreateOptionsMenu(Menu menu) {
+        // Inflate the menu; this adds items to the action bar if it is present.
+        getMenuInflater().inflate(R.menu.main, menu);
+        return true;
+    }
+
+
+	@Override
+	public void onClick(View v) {
+		// TODO Auto-generated method stub
+		switch(v.getId())
+		{
+		case R.id.btstart:
+			Intent llama_pantalla_datos=new Intent("com.example.android_application.Pantalladatosusuarios");
+			startActivity(llama_pantalla_datos);
+		}
+		
+	}
+
+
+    
+}
---
+++ Android_application/src/com/example/android_application/Pantalladatosusuarios.java
@@ -0,0 +1,15 @@
+package com.example.android_application;
+
+import android.app.Activity;
+import android.os.Bundle;
+
+public class Pantalladatosusuarios extends Activity{
+
+	@Override
+	protected void onCreate(Bundle savedInstanceState) {
+		// TODO Auto-generated method stub
+		super.onCreate(savedInstanceState);
+		setContentView(R.layout.pantalladatosusuario);
+	}
+
+}
